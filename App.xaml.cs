208e5d13d1e0a45fb87a7d411537c3978aaa585a
ali_butun <abutun26@gmail.com>
2025-01-22 00:45:16 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiApp2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            // MainPage'i kök sayfa olarak ayarla
+            return new Window(new MainPage());
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using NModbus;
+using System.Net.Sockets;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnStartButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                // Modbus TCP bağlantısını başlat
+                string plcIpAddress = "192.168.0.1"; // PLC IP adresi
+                int plcPort = 502; // Modbus TCP portu
+
+                using (TcpClient client = new TcpClient(plcIpAddress, plcPort))
+                {
+                    // Modbus TCP Master oluştur
+                    var factory = new ModbusFactory();
+                    var master = factory.CreateMaster(client);
+
+                    // Slave ID (default olarak 1 kullanılır)
+                    byte slaveId = 1;
+
+                    // Coil 0 (%I0.0) adresini TRUE olarak ayarla
+                    ushort coilAddress = 0; // Coil adresi (0)
+                    master.WriteSingleCoil(slaveId, coilAddress, true);
+
+                    // Başarı mesajı
+                    await DisplayAlert("Başarılı", "%I0.0 başarıyla set edildi!", "Tamam");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata mesajı
+                await DisplayAlert("Hata", $"PLC'ye bağlanırken hata oluştu: {ex.Message}", "Tamam");
+            }
+        }
+    }
+}
